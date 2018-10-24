dfeb63af5905faa318cb56e4ab2036e83409afe8
Nick <drbiao@outlook.com>
2018-10-24 21:12:47 +1100

    add button

--- app/src/main/java/com/example/cris/quizapp/MainActivity.java
+++ app/src/main/java/com/example/cris/quizapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.cris.quizapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // define the button
+        Button btnStart = findViewById(R.id.button_start);
+
+        // create onClickListener for button
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startQuiz();
+            }
+        });
     }
+
+    // method move to video intent and start the quiz
+    public void startQuiz(){
+        Intent intent = new Intent();
+    }
+
+
 }
+
